09b8dc8c28f5f91cbf4680cf5236a7fa7f7324c4
Bhakti Jadhav <bjadhav@agivant.com>
2025-04-01 16:05:00 +0530

    V1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import Navbar from "@/components/Navbar"
+import { ThemeProvider } from "next-themes"
 
 export default function RootLayout({
   children,
@@ -12,7 +14,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
