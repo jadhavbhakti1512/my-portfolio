068300950b6380edf3dba0ed3b90d48978809759
bhakti <jadhavbhakti1512@gmail.com>
2025-09-24 01:02:37 +0530

    about me sequence changed

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <HeroSection />
-      <AboutSection />
       <SkillsSection />
       <Projects />
       <ProjectsSection />
+      <AboutSection />
       <Contact />
       <FAQChatbot /> 
        
